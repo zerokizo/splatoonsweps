f88a71060091434ab8f717b41a9714fbbf21dcfd
ZenkakuHiragana <daiko1997@yahoo.co.jp>
2020-04-29 15:12:54 +0900

    Improved blaster explosion

--- lua/effects/SplatoonSWEPsBlasterExplosion.lua
+++ lua/effects/SplatoonSWEPsBlasterExplosion.lua
@@ -1,14 +1,39 @@
 
 local ss = SplatoonSWEPs
 if not ss then return end
-local mdl = Model "models/props_junk/PopCan01a.mdl"
+local mdl = Model "models/hunter/misc/sphere2x2.mdl"
+local mat = Material "splatoonsweps/effects/explosion_ink"
 function EFFECT:Init(e)
+	local maxscale = e:GetRadius() / 120
+	local minscale = maxscale / 2
+	local color = ss.GetColor(e:GetColor())
 	self:SetModel(mdl)
-	self:SetMaterial(ss.Materials.Effects.Invisible)
-	self:SetNoDraw(true)
-	local c = ss.GetColor(e:GetColor())
-	local p = CreateParticleSystem(game.GetWorld(), ss.Particles.Explosion, PATTACH_WORLDORIGIN, 0, e:GetOrigin())
-	p:AddControlPoint(1, game.GetWorld(), PATTACH_WORLDORIGIN, nil, c:ToVector())
+	self:SetMaterial "splatoonsweps/effects/explosion_ink"
+	self:SetPos(e:GetOrigin())
+	self:SetAngles(AngleRand())
+	self:SetColor(color)
+	self:SetModelScale(minscale)
+	self.Frame = 0
+	self.InitTime = CurTime()
+	self.MaxScale = maxscale
+	self.MinScale = minscale
+	local p = CreateParticleSystem(game.GetWorld(), ss.Particles.Explosion, PATTACH_WORLDORIGIN, 0, self:GetPos())
+	p:AddControlPoint(1, game.GetWorld(), PATTACH_WORLDORIGIN, nil, color:ToVector())
 	p:AddControlPoint(2, game.GetWorld(), PATTACH_WORLDORIGIN, nil, vector_up * e:GetRadius())
 	sound.Play(e:GetFlags() > 0 and "SplatoonSWEPs.BlasterHitWall" or "SplatoonSWEPs.BlasterExplosion", e:GetOrigin())
+end
+
+local numframes = 32
+function EFFECT:Think()
+	local t = CurTime() - self.InitTime
+	local f = math.floor(t * ss.SecToFrame * 3)
+	local frac = math.EaseInOut(f / (numframes - 1), 0.3, 0.3)
+	self.Frame = math.Clamp(f, 0, numframes - 1)
+	self:SetModelScale(Lerp(frac, self.MinScale, self.MaxScale))
+	return f < numframes
+end
+
+function EFFECT:Render()
+	mat:SetInt("$frame", self.Frame)
+	self:DrawModel()
 end
