d82a2dd1d3cdaf5313b872906dfb78cc25402ef5
ZenkakuHiragana <daiko1997@yahoo.co.jp>
2020-06-24 02:52:58 +0900

    Splat bomb: improved behavior

--- lua/entities/ent_splatoonsweps_splatbomb.lua
+++ lua/entities/ent_splatoonsweps_splatbomb.lua
@@ -5,13 +5,20 @@ AddCSLuaFile()
 
 ENT.Type = "anim"
 
-local mdl = Model "models/props_splatoon/weapons/subs/splat_bombs/splat_bomb.mdl"
+local Fly_AirFrm = 4 * ss.FrameToSec
+local mdl = Model "models/splatoonsweps/subs/splat_bomb/splat_bomb.mdl"
 function ENT:Initialize()
     self:SetModel(mdl)
     self:SetCollisionGroup(COLLISION_GROUP_WEAPON)
+    self.DragCoeffChangeTime = CurTime() + Fly_AirFrm
+    self.DragCoefficient = 0
+    self.TraceVector = -vector_up * self:BoundingRadius() / 2
     if CLIENT then return end
     self:PhysicsInit(SOLID_VPHYSICS)
     self:PhysWake()
+    local p = self:GetPhysicsObject()
+    if not IsValid(p) then return end
+    p:EnableDrag(false)
 end
 
 function ENT:SetupDataTables()
@@ -19,8 +26,8 @@ function ENT:SetupDataTables()
 end
 
 if CLIENT then return end
-
 function ENT:Detonate()
+    if self.RemoveFlag then return end
     if not self.DetonateTime then return end
     if CurTime() < self.DetonateTime then return end
     ss.MakeBombExplosion(self:GetPos(), self.Owner, self:GetNWInt "inkcolor")
@@ -28,9 +35,37 @@ function ENT:Detonate()
 end
 
 function ENT:Think()
-    if util.QuickTrace(self:GetPos(), -vector_up * 12, self).Hit then self:Detonate() end
+    if CurTime() > self.DragCoeffChangeTime then
+        local p = self:GetPhysicsObject()
+        if IsValid(p) then
+            local intensity = CurTime() - self.DragCoeffChangeTime
+            self.DragCoefficient = intensity * 50
+            p:EnableDrag(true)
+            p:SetDragCoefficient(self.DragCoefficient)
+            p:SetAngleDragCoefficient(self.DragCoefficient)
+        end
+    end
+
+    if util.QuickTrace(self:GetPos(), self.TraceVector, self).Hit then
+        self:Detonate()
+    end
+
     if not self.RemoveFlag then return end
     self:Remove()
+end
+
+function ENT:PhysicsUpdate(p) -- Apply 1.5x gravity
+    if CurTime() < self.DragCoeffChangeTime then return end
+    local g = physenv.GetGravity()
+    local g_dir = g:GetNormalized()
+    local g_len = g:Length()
+    local g_desired = 0.16 * ss.ToHammerUnitsPerSec2
+    local g_amount = g_desired - g_len
+    local m = p:GetMass()
+    local dt = FrameTime()
+    local drag = self.DragCoefficient * p:GetVelocity().z
+    p:ApplyForceCenter(g_dir * g_amount * m * dt)
+    p:ApplyForceCenter(vector_up * drag * dt)
 end
 
 function ENT:PhysicsCollide(data, collider)
--- lua/splatoonsweps/sub/splatbomb.lua
+++ lua/splatoonsweps/sub/splatbomb.lua
@@ -4,14 +4,78 @@ AddCSLuaFile()
 local ss = SplatoonSWEPs
 if not ss then return {} end
 
+-- Parameters are from Splatoon 2 ver. 5.2.0, https://leanny.github.io/splat2new/parameters.html
 local module = {}
-local InkConsume = 70
+local p = {
+    BringCloseRateCrossVec = 0.7,
+    BringCloseRateMoveVec = 0.7,
+    Burst_Damage_Far = 0.3,
+    Burst_Damage_Near = 1.8,
+    Burst_KnockBackCoreImpact = 3,
+    Burst_KnockBackKb = 0.8,
+    Burst_KnockBackRadius = 120,
+    Burst_PaintR = 40,
+    Burst_Radius_Far = 70,
+    Burst_Radius_Near = 40,
+    Burst_SeFrm = 10,
+    Burst_SplashAroundFuzzyRate = 2,
+    Burst_SplashNum = 15,
+    Burst_SplashOfstY = 5,
+    Burst_SplashPaintR = 10,
+    Burst_SplashPitH = 45,
+    Burst_SplashPitL = 5,
+    Burst_SplashVelH = 5.4,
+    Burst_SplashVelL = 4.8,
+    Burst_WaitFrm = 30,
+    Burst_WarnFrm = 30,
+    CollisionSeSilentFrame = 4,
+    CollisionSeVelDotGndNrm = 0.08,
+    CollisionSeVelDotGndTimes = 1.2,
+    CrossPaintRadius = 20,
+    CrossPaintRayLength = 20,
+    CrossPaintRayRadius = 1,
+    EffectShakeRange = 100,
+    Fly_AirFrm = 4,
+    Fly_CeilingKf_Y = 1,
+    Fly_CeilingKr_Y = 0.5,
+    Fly_Gravity = 0.16,
+    Fly_InitPit = 0.4,
+    Fly_InitRol = 0.12,
+    Fly_RotI = 20,
+    Fly_RotKd = 0.98015,
+    Fly_VelKd = 0.94134,
+    Fly_WallKf_XZ = 0.7,
+    Fly_WallKf_Y = 0.2,
+    Fly_WallKr = 0.2,
+    ForceSleepFrame = 900,
+    InkConsume = 0.7,
+    Land_AbsorveRt = 0.1,
+    Land_GndKf = 0.2,
+    Land_GndKr = 0.05,
+    Land_GravGndK = 0.5,
+    Land_GravH = 0.08,
+    Land_GravKf = 0.1,
+    Land_GravL = 0.016,
+    Land_NrmGndKf = 0.1,
+    Land_RotI = 10,
+    Land_RotKd = 0.96059,
+    Land_VelKdXZ = 0.92237,
+    Land_VelKdY = 0.92237,
+    Shape_SphereD = 5,
+    Shape_SphereR = 2,
+
+    -- Parameters from Player_Spec_BombDistance_Up.param
+    BombThrow_VelZ_Low = 2.8,
+    BombThrow_VelZ_Mid = 3.5,
+    BombThrow_VelZ_High = 4.2,
+}
+
 function module:SharedSecondaryAttack(throwable)
     
 end
 
 function module:CanSecondaryAttack()
-    return self:GetInk() > InkConsume
+    return self:GetInk() > p.InkConsume * ss.MaxInkAmount
 end
 
 if SERVER then
@@ -23,9 +87,18 @@ if SERVER then
         e:SetInkColorProxy(self:GetInkColorProxy())
         e:SetPos(self:GetShootPos())
         e:Spawn()
-        e:GetPhysicsObject():ApplyForceCenter(self:GetAimVector() * 20000)
-        e:GetPhysicsObject():ApplyTorqueCenter(self:GetRight() * -270)
-        self:SetInk(math.max(0, self:GetInk() - InkConsume))
+
+        local ph = e:GetPhysicsObject()
+        if IsValid(ph) then
+            local dir = self:GetAimVector()
+            local speed_amount = p.BombThrow_VelZ_Low * ss.ToHammerUnitsPerSec * ph:GetMass()
+            local speed_mul = 1.6 -- WORKAROUND: This should be 1, but it's too slow
+            speed_amount = speed_amount * speed_mul
+            ph:ApplyForceCenter(dir * speed_amount + vector_up * speed_amount * 0.2)
+            ph:ApplyTorqueCenter(self:GetRight() * -540)
+        end
+
+        self:SetInk(math.max(0, self:GetInk() - p.InkConsume * ss.MaxInkAmount))
         self:SetReloadDelay(40 * ss.FrameToSec)
     end
 else
