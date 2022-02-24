c266a0873511bcfb164f83e8f2238b8370baaebb
ZenkakuHiragana <daiko1997@yahoo.co.jp>
2022-02-25 01:18:33 +0900

    Added feature of angle change

--- lua/splatoonsweps/client/minimap.lua
+++ lua/splatoonsweps/client/minimap.lua
@@ -1,6 +1,9 @@
 
 local ss = SplatoonSWEPs
 if not ss then return end
+
+local toggle = true
+local cos, sin, rad = math.cos, math.sin, math.rad
 function ss.OpenMiniMap()
     local bb
     for i, t in ipairs(ss.MinimapAreaBounds) do
@@ -11,57 +14,78 @@ function ss.OpenMiniMap()
     end
 
     if not bb then return end
+    local inclined = true
+    local inclinedYaw = 30
+    local inclinedPitch = 30
+    local angleRate = 90
+    local upAngle = Angle(90, 0, 0)
+    local inclinedAngle = Angle(90 - inclinedPitch, inclinedYaw, 0)
+    local desiredAngle = Angle(inclinedAngle)
+    local currentAngle = Angle(desiredAngle)
     local mins, maxs = bb.mins, bb.maxs
-    local msx = maxs.y - mins.y
-    local msy = maxs.x - mins.x
-    local msxy = math.max(msx, msy)
-    local shiftx = (maxs.x + mins.x) / 2
-    local shifty = (maxs.y + mins.y) / 2
-    local shiftz = maxs.z + 1
-    local orthoscale = msxy / 2
-
+    local bbsize = maxs - mins
+    local org = Vector(mins.x, mins.y, maxs.z + 1)
     local props = vgui.Create("DProperties")
     local frame = vgui.Create("DFrame")
+    local panel = vgui.Create("DButton", frame)
     props:Dock(FILL)
-    frame:SetSize(900 * msx / msy, 900)
+    frame:SetSizable(true)
+    frame:SetSize(ScrH(), ScrH())
     frame:Center()
     frame:MakePopup()
     frame:SetTitle("Splatoon SWEPs: Minimap")
-    frame.Paint = function(s, w, h)
-        surface.SetDrawColor(118, 200, 227, 100)
-        surface.DrawRect(0, 0, w, h)
-    end
-
-    local panel = vgui.Create("DButton", frame)
     panel:Dock(FILL)
     panel:SetText("")
-    panel:SetSize(600, 600)
 
-    local toggle = true
     local keydown = input.IsKeyDown(KEY_LSHIFT)
-    panel.Think = function(s)
+    function panel:Think()
         local k = input.IsKeyDown(KEY_LSHIFT)
         if Either(toggle, not keydown and k, not k) then frame:Close() end
         keydown = k
     end
 
-    panel.Paint = function(s, w, h)
-        local x, y = s:GetPos()
-        local x1, y1 = s:GetParent():GetPos()
-        x, y = x + x1, y + y1
+    function panel:DoClick()
+        inclined = not inclined
+        desiredAngle = inclined and inclinedAngle or upAngle
+    end
+
+    function panel:Paint(w, h)
+        currentAngle.yaw = math.ApproachAngle(currentAngle.yaw, desiredAngle.yaw, angleRate * RealFrameTime())
+        currentAngle.pitch = math.ApproachAngle(currentAngle.pitch, desiredAngle.pitch, angleRate * RealFrameTime())
+        local x, y = self:LocalToScreen(0, 0)
+        local left = -bbsize.y * cos(rad(currentAngle.yaw))
+        local right = bbsize.x * sin(rad(currentAngle.yaw))
+        local top = -bbsize.z * sin(rad(90 - currentAngle.pitch))
+        local bottom = bbsize.x * cos(rad(currentAngle.yaw)) + bbsize.y * sin(rad(currentAngle.yaw)) + bbsize.z * sin(rad(90 - currentAngle.pitch))
+        local width = right - left
+        local height = bottom - top
+        local aspectratio = w / h
+        local addMarginAxisY = width > height
+        if aspectratio < 1 then addMarginAxisY = not addMarginAxisY end
+        if addMarginAxisY then
+            local diff = width / aspectratio - height
+            local margin = diff / 2
+            top = top - margin
+            bottom = bottom + margin
+        else
+            local diff = height * aspectratio - width
+            local margin = diff / 2
+            left = left - margin
+            right = right + margin
+        end
         ss.IsDrawingMinimap = true
         render.RenderView {
             drawviewmodel = false,
-            origin = Vector(shiftx, shifty, shiftz),
-            angles = Angle(90, 0, 0),
-            aspectratio = msx / msy,
+            origin = org,
+            angles = currentAngle,
+            aspectratio = aspectratio,
             x = x, y = y,
             w = w, h = h,
             ortho = {
-                left   = -1 * orthoscale,
-                right  =  1 * orthoscale,
-                top    = -1 * orthoscale * msy / msx,
-                bottom =  1 * orthoscale * msy / msx,
+                left   = left,
+                right  = right,
+                top    = top,
+                bottom = bottom,
             },
             znear = 1,
             zfar = 32768,
